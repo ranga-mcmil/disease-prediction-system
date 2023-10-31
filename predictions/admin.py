b005415b4a32c6465fc6c48e2169a28fcacb01f7
Ranga McMil <81158734+ranga-mcmil@users.noreply.github.com>
2023-10-31 09:27:17 +0200

    adding excel exports

--- predictions/admin.py
+++ predictions/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Prediction
+from import_export.admin import ExportActionMixin
+
 
 # Register your models here.
 @admin.register(Prediction)
-class PredictionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'symptom1', 'symptom2', 'symptom3', 'symptom4', 'symptom5')
+class PredictionAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('id', 'created_by', 'symptom1', 'symptom2', 'symptom3', 'symptom4', 'symptom5', 'result', )
+    list_filter = ('created_by',)
+    search_fields = ('created_by', )
+    raw_id_fields = ('created_by',)
